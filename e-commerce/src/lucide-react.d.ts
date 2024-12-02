3c54ca9fd27dac6a171f1b51f59b906c1a4b9030
komathikm <kmkomathi_jtbb2_jtdfoundation.org>
2024-12-02 10:46:35 +0530

    first commit

---
+++ e-commerce/src/lucide-react.d.ts
@@ -0,0 +1,9 @@
+declare module 'lucide-react' {
+    export const Star: React.FC<React.SVGProps<SVGSVGElement>>;
+    // Add more icons as needed
+  }
+  
+
+import * as Lucide from "lucide-react";
+
+const { Trash2 } = Lucide;
